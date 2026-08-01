0d6b3c7d5bb431ece960135fc2c7bcdbb136a16b
Kieu Trung Tin <tinbon2406@gmail.com>
2026-08-01 18:56:00 +0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map();
+
+        for(let num of nums){
+            if(!map.has(num)){
+                map.set(num, 0);
+            }
+            map.set(num, map.get(num) + 1);
+        }
+
+        let array = new Array();
+        for(let [key, value] of map){
+            array.push([key,value]);
+        }
+        array.sort((a, b) => b[1] - a[1]);
+
+        let result = [];
+        for(let i = 0; i < k; i++){
+            result.push(array[i][0]);
+        }
+
+        return result;
+    }
+}
