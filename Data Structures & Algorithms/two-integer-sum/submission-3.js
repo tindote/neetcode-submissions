cfce248da75e933eeee2d9d62aac0660dc03d5e6
Kieu Trung Tin <tinbon2406@gmail.com>
2026-08-01 17:16:09 +0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let set = new Set();
+        for(let i = 0 ; i < nums.length - 1 ; i++){
+            if(!set.has(nums[i]) ){
+                for(let j = i + 1; j < nums.length ; j++){
+                    if (nums[i] + nums[j] === target){
+                        return [i,j];
+                    }
+                }
+                set.add(nums[i]);
+            }
+        }
+    }
+}
