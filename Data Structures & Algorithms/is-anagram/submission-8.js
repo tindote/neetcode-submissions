f1a64820d4bf1570f7343102b0b1836cfdfc06a9
Kieu Trung Tin <tinbon2406@gmail.com>
2026-08-01 17:08:27 +0700

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length != t.length){
+            return false;
+        }
+
+        let chars = Array(26).fill(0);
+
+        for(let i = 0; i < s.length; i++){
+            chars[s.charCodeAt(i) - 97]++;
+            chars[t.charCodeAt(i) - 97]--;
+        }
+
+        for(let el of chars){
+            if(el != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
